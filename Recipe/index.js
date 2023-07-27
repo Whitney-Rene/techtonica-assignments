ba03dfce1358159ab07f3910f151eb62a52cf1dc
Whitney-Rene <whtnchambers76@gmail.com>
2023-07-27 16:45:46 -0400

    new js file and updates for css file

---
+++ Recipe/index.js
@@ -0,0 +1,10 @@
+
+let newListItems = document.createElement("li");
+newListItems.textContent = "don't forget to add LOTS OF LOVE!";
+document.querySelector("ul").appendChild(newListItems);
+
+let newInstruction = document.createElement("li");
+newInstruction.textContent = "Let cool and ENJOY!";
+document.getElementById("Instructions").appendChild(newInstruction);
+
+const instructions = document.getElementById("Instructions");
