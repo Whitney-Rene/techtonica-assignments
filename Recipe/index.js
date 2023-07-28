d3ecb8c23a4f53ca6c9f69d585e3e09e94a01d83
Whitney-Rene <whtnchambers76@gmail.com>
2023-07-27 23:41:26 -0400

    change photo

--- Recipe/index.js
+++ Recipe/index.js
@@ -1,10 +1,14 @@
 
 let newListItems = document.createElement("li");
 newListItems.textContent = "don't forget to add LOTS OF LOVE!";
+newListItems.style.color = "#505050";
+newListItems.style.fontWeight = "bold";
 document.querySelector("ul").appendChild(newListItems);
 
 let newInstruction = document.createElement("li");
 newInstruction.textContent = "Let cool and ENJOY!";
+newInstruction.style.color = "#505050";
+newInstruction.style.fontWeight = "bold";
 document.getElementById("Instructions").appendChild(newInstruction);
 
-const instructions = document.getElementById("Instructions");
+let otherLinks = document.getElementById("OtherLinks"); 
