6398560f0492ba4e380d3a64869ac5ccd2216d83
Whitney-Rene <whtnchambers76@gmail.com>
2023-08-04 09:56:50 -0400

    attempts to add a console log feature for user input

---
+++ myPortfolio/index.js
@@ -0,0 +1,11 @@
+document.getElementById("submit").addEventListener("click", (event) =>{
+    event.preventDefault();
+
+    //Grabs the inputs
+    const username = document.getElementById("fnameinput").value;
+    const email = document.getElementById("email").value;
+
+    //console.log
+    console.log(username);
+    console.log(email);
+})
