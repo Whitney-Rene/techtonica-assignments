931980b1b6b1496c0a6fa8d0a33fb2ab2f23ae4c
Whitney-Rene <whtnchambers76@gmail.com>
2023-08-04 15:04:08 -0400

    fixes required fields

--- myPortfolio/script.js
+++ myPortfolio/script.js
@@ -1,9 +1,36 @@
+// let userInput = document.getElementById('fnameinput')
+// userInput.setAttribute("required", "required");
+
+let submit = document.getElementById("submit");
+console.log(submit);
+
 document.getElementById("submit").addEventListener("click", (event) =>{
     event.preventDefault();
 
     //Grabs the inputs
     const username = document.getElementById("fnameinput").value;
     const email = document.getElementById("email").value;
+
+
+    //validate username is not empty
+    const divError = document.getElementById("emailError");
+    if(username === "" || email === ""){
+        divError.innerHTML = "Please enter both fields"
+    } else{
+        divError.innerHTML = ""
+    }
+    
+    submit.appendChild(divError);
+
+
+    // //Email validation - validate that the pattern in the email is correct
+    // const secErrorDiv = document.getElementsByClassName("emailError");
+    // const correctPattern =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // if(!correctPattern.test(email)){
+    //     secErrorDiv.innerText = "Please enter a valid email"
+    // } else{
+    //     secErrorDiv.innerText = ""
+    // }
 
     //console.log
     console.log(username);
