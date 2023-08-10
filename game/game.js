8c45d02a5c8462135e53e4b3a4d4c2fbe7b6b60b
Whitney-Rene <whtnchambers76@gmail.com>
2023-08-10 14:16:33 -0400

    chooses different colors for output, changes title color

--- game/game.js
+++ game/game.js
@@ -7,18 +7,21 @@ btn.addEventListener("click", function(){
     let input = document.getElementById("userInput").value;
     if(input == "") {
         output.innerHTML = "you must enter something"
-        output.style.color = "hotpink";
+        output.style.color = "navy";
     } else if (input <= 0 || input > 5){
         output.innerHTML = "remember to guess between numbers 1 - 5"
-        output.style.color = "hotpink";
+        output.style.color = "dimgray";
     } else if (input < number){
         output.innerHTML = "too low";
-        output.style.color = "hotpink";
+        output.style.color = "white";
     } else if (input > number){
         output.innerHTML = "too high";
-        output.style.color = "hotpink";
+        output.style.color = "saddlebrown";
     } else if(input == number){
         output.innerHTML = `You guessed correctly, the number was ${number}`
         output.style.color = "hotpink";
     }
 });
+
+//play again button?? / how to reset the game? 
+//what if people don't enter a number?
