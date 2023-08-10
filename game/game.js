ac06a0fc0cd451efb629264a2aeba1fb175299fa
Whitney-Rene <whtnchambers76@gmail.com>
2023-08-10 13:47:27 -0400

    adds if/else statements to js code

--- game/game.js
+++ game/game.js
@@ -3,9 +3,14 @@ let output = document.getElementById("output");
 
 let number = Math.floor(Math.random() * 10) + 1;
 
-btn.addEventListener("clcik", function(){
+btn.addEventListener("click", function(){
     let input = document.getElementById("userInput").value;
     if(input == number){
-        output.innerHTML = `You guessed correctly, your number was ${number}`
-    } else if ()
-})
+        output.innerHTML = `You guessed correctly, the number was ${number}`
+    } else if (input < number){
+        output.innerHTML = "too low";
+    }
+    if (input > number){
+        output.innerHTML = "too high";
+    }
+});
