a944de73db8aeb1579e4dc1f06ab33ba13ea2436
Whitney-Rene <whtnchambers76@gmail.com>
2023-08-10 14:05:27 -0400

    changes output color to hot pink

--- game/game.js
+++ game/game.js
@@ -7,13 +7,18 @@ btn.addEventListener("click", function(){
     let input = document.getElementById("userInput").value;
     if(input == "") {
         output.innerHTML = "you must enter something"
+        output.style.color = "hotpink";
     } else if (input <= 0 || input > 5){
         output.innerHTML = "remember to guess between numbers 1 - 5"
+        output.style.color = "hotpink";
     } else if (input < number){
         output.innerHTML = "too low";
+        output.style.color = "hotpink";
     } else if (input > number){
         output.innerHTML = "too high";
+        output.style.color = "hotpink";
     } else if(input == number){
         output.innerHTML = `You guessed correctly, the number was ${number}`
+        output.style.color = "hotpink";
     }
 });
